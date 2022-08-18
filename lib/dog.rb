0b6f0b1f627fd9e7fb469c582c89bbda648b762b
Sieva-cmd <musyokasieva@gmail.com>
2022-08-18 18:02:59 +0300

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
-
-end
+    #setter method fir name
+     def name=(name)
+       @name=name
+     end
+     #setter method for breed
+     def breed=(breed)
+     @breed=breed
+  end
+  
+   #getter method for name
+  def name
+    @name
+   end 
+   #getter method for breed
+    def breed
+    @breed
+    end
+end 
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
-
+    #setter method fir name
+    def name=(name)
+        @name=name
+      end
+      #setter method for breed
+      def job=(job)
+      @job=job
+   end
+   
+    #getter method for name
+   def name
+     @name
+    end 
+    #getter method for breed
+     def job
+     @job
+     end
 end
